a974119a0e1538299e489ba5cfe27843a42e6dbd
Mitchell Lisle <lislemitchell@gmail.com>
2024-12-15 17:46:41 +1100

    add llm summary back

--- src/data/matchResults.json.js
+++ src/data/matchResults.json.js
@@ -11,17 +11,17 @@ async function run(premierLeagueAPI, summariser) {
   const matchResultsWithCumsum = await premierLeagueAPI.getCumulativeSum(matchResults);
   const matchResultsWithCumsumRankings = await premierLeagueAPI.getRankingsForGameweeks(matchResultsWithCumsum);
 
-  // const title = await summariser.chat(
-  //     `Give me a title for this data. Decide who the most relevant person to focus on in the title.
-  //     Focus on the most recent gameweek and alternate between all the users. Don't give me stats or numbers, give me
-  //     the type of title you'd see on a 1950s old timey newspaper. No more than 8 words`,
-  //     matchResultsWithCumsumRankings
-  // );
-  //
-  // const sentence = await summariser.summarise(matchResultsWithCumsum);
+  const title = await summariser.chat(
+      `Give me a title for this data. Decide who the most relevant person to focus on in the title.
+      Focus on the most recent gameweek and alternate between all the users. Don't give me stats or numbers, give me
+      the type of title you'd see on a 1950s old timey newspaper. No more than 8 words`,
+      matchResultsWithCumsumRankings
+  );
 
-  const title = `Sample title`;
-  const sentence = `Sample sentence`;
+  const sentence = await summariser.summarise(matchResultsWithCumsum);
+
+  // const title = `Sample title`;
+  // const sentence = `Sample sentence`;
 
   return {
       title: title,
